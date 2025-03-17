371798f4d34f63fe225d6409f785ca13c6ab91ba
Matteo Brighi <brighi.matteo@gmail.com>
2025-03-18 00:57:30 +0100

    CONSTANT: aggiunta costante DAYS_IN_MONTH

--- game/constants.lua
+++ game/constants.lua
@@ -35,6 +35,19 @@ constants.MOUSE_SIZE          = 64
 constants.DEFAULT_CURSOR      = "default"
 constants.HAND_CURSOR         = "hand"
 constants.HAND_CLICKED_CURSOR = "hand_clicked"
-
+constants.DAYS_IN_MONTH = {               --numero di giorni per ogni mese
+    [1] = 31,  -- January
+    [2] = 28,  -- February
+    [3] = 31,  -- March
+    [4] = 30,  -- April
+    [5] = 31,  -- May
+    [6] = 30,  -- June
+    [7] = 31,  -- July
+    [8] = 31,  -- August
+    [9] = 30,  -- September
+    [10] = 31, -- October
+    [11] = 30, -- November
+    [12] = 31  -- December
+}
 
 return constants
