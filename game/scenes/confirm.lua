40cd2b65c537547c9f85f67c8c7d22e009855bd1
dyd <riccardo.pancotti@gmail.com>
2025-03-22 11:59:56 +0100

    feat: implement clickable buttons in confirm and outcome scenes

--- game/scenes/confirm.lua
+++ game/scenes/confirm.lua
@@ -1,9 +1,41 @@
 local constants = require("constants")
 
 confirm = {}
+local yesButtonArea = {}
+local noButtonArea = {}
+
+local function yesButtonHandler()
+  print("Yes button clicked")
+end
+
+local function noButtonHandler()
+  print("No button clicked")
+end
 
 function confirm.load()
-  mouse.registerHandler(agenda, constants.SCENES_CONFIRM)
+  mouse.registerHandler(confirm, constants.SCENES_CONFIRM)
+
+  yesButtonArea = screenManager:calcAreaSizes({
+    xPerc = 0.45,
+    yPerc = 0.7,
+    widthPerc = 0.1,
+    heightPerc = 0.1
+  })
+
+  noButtonArea = screenManager:calcAreaSizes({
+    xPerc = 0.65,
+    yPerc = 0.7,
+    widthPerc = 0.1,
+    heightPerc = 0.1
+  })
+
+  screenManager:setClickableArea(constants.SCENES_CONFIRM, yesButtonArea, constants.SCENES_CONFIRM, function()
+    yesButtonHandler()
+  end)
+
+  screenManager:setClickableArea(constants.SCENES_CONFIRM, noButtonArea, constants.SCENES_CONFIRM, function()
+    noButtonHandler()
+  end)
 end
 
 function confirm.update(dt)
@@ -11,6 +43,9 @@ end
 
 function confirm.draw()
   -- screenManager:drawSceneBackground(constants.IMAGES_OUTCOME_WIN_SESSION)
+
+  love.graphics.rectangle("line",yesButtonArea.x,yesButtonArea.y,yesButtonArea.width,yesButtonArea.height)
+  love.graphics.rectangle("line",noButtonArea.x,noButtonArea.y,noButtonArea.width,noButtonArea.height)
 end
 
 function confirm.keypressed(key)
@@ -22,6 +57,7 @@ function confirm.keypressed(key)
 end
 
 function confirm.mousePressed(x, y, button)
+  screenManager:checkIfIsClickable(x, y)
 end
 
 function confirm.mouseHovered(x, y)
--- game/scenes/outcome.lua
+++ game/scenes/outcome.lua
@@ -1,17 +1,39 @@
 local constants = require("constants")
 
 outcome = {}
+local okButtonArea = {}
+
+-- Il click su OK ci porterá su una scena diversa, in base all'esito della giocata
+
+local function okButtonHandler()
+  -- Navigo su title se GAME WIN - GAME OVER
+  -- Navigo su DESKTOP se SESSIONE WIN -- SESSION OVER
+end
 
 function outcome.load()
-  mouse.registerHandler(agenda, constants.SCENES_OUTCOME)
+  mouse.registerHandler(outcome, constants.SCENES_OUTCOME)
+  okButtonArea = screenManager:calcAreaSizes({
+    xPerc = 0.45,
+    yPerc = 0.7,
+    widthPerc = 0.1,
+    heightPerc = 0.1
+  })
+
+  screenManager:setClickableArea(constants.SCENES_OUTCOME, okButtonArea, constants.SCENES_OUTCOME, function()
+    okButtonHandler()
+  end)
 end
 
 function outcome.update(dt)
 end
 
 function outcome.draw()
+  love.graphics.rectangle("line",okButtonArea.x,okButtonArea.y,okButtonArea.width,okButtonArea.height)
+
   -- Disegnare sfondo in base ad esito giocata
-  local outcome = GameManager:getGameOutcome()
+  local outcome = GameManager:getOutcomeState()
+
+  print("Outcome: " .. outcome)
 
   if outcome == constants.OUTCOMESTATE[1] then
     -- -- Session WIN
@@ -37,6 +59,7 @@ function outcome.keypressed(key)
 end
 
 function outcome.mousePressed(x, y, button)
+  screenManager:checkIfIsClickable(x, y)
 end
 
 function outcome.mouseHovered(x, y)
