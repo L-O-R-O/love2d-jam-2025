7de95d59e9c3297f59d75edb8fae413d9dc5567a
dyd <riccardo.pancotti@gmail.com>
2025-03-23 13:17:13 +0100

    update outcome images and modify server script directory path

--- tools/serve-web.sh
+++ tools/serve-web.sh
@@ -35,7 +35,7 @@ echo "Extracting to temporary directory..."
 unzip -q "$ZIP_FILE" -d "$TEMP_DIR"
 
 echo "Starting server on http://localhost:1337"
-cd "$TEMP_DIR/weHaveToReschedule"
+cd "$TEMP_DIR/"
 miniserve \
   --header "Cross-Origin-Opener-Policy: same-origin" \
   --header "Cross-Origin-Embedder-Policy: require-corp" \
